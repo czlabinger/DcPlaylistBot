f346a899b9c479ea762a279d434b97480f473d1e
czlabinger <stoffi05@pm.me>
2025-07-09 20:59:11 +0200

    Restart with new aproach

---
+++ DcPlaylistPlugin/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using IPA;
+using IPA.Loader;
+using IpaLogger = IPA.Logging.Logger;
+
+namespace DcPlaylistPlugin;
+
+[Plugin(RuntimeOptions.DynamicInit)]
+internal class Plugin {
+    internal static IpaLogger Log { get; private set; } = null!;
+
+    [Init]
+    public Plugin(IpaLogger ipaLogger, PluginMetadata pluginMetadata) {
+        Log = ipaLogger;
+        Log.Info($"{pluginMetadata.Name} {pluginMetadata.HVersion} initialized.");
+    }
+
+    [OnStart]
+    public void OnApplicationStart() {
+        Log.Debug("OnApplicationStart");
+    }
+
+    [OnExit]
+    public void OnApplicationQuit() {
+        Log.Debug("OnApplicationQuit");
+    }
+}
---
+++ PlaylistBot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+
+public class Program {
+    
+    private static DiscordSocketClient? _client;
+    
+    public static async Task Main() {
+        _client = new DiscordSocketClient();
+        
+        _client.Log += Log;
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        string resourceName = "PlaylistBot.token.txt";
+        string token = "";
+        
+        using (Stream? stream = assembly.GetManifestResourceStream(resourceName))
+        {
+            if (stream == null)
+            {
+                throw new InvalidOperationException($"Resource '{resourceName}' not found.");
+            }
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                token = reader.ReadToEnd();
+                
+            }
+        }
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        AppDomain.CurrentDomain.ProcessExit += async (sender, eventArgs) =>
+        {
+            Console.WriteLine("Process is exiting!");
+            await _client.LogoutAsync();
+            await _client.StopAsync();
+        };
+        
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg) {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
